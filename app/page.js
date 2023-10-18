f70b1228f5df0c65f32a0c86f547d73f793cfb5f
Will Nickson <wfgnickson@gmail.com>
2023-10-18 18:46:34 +0100

    update main page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <main className="px-6 mx-auto">
+            <p className="mt-12 mb-12 text-3xl text-center">Hello and Welcome 👋</p>
+            <p className="mt-12 mb-12 text-2xl text-center">{"I'm Will"}</p>
+            <Posts />
+        </main>
+    );
 }
