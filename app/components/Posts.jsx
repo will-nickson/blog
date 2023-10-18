0d43ff9bc56889e7588f4eb1f46f83dc58abd5d8
Will Nickson <wfgnickson@gmail.com>
2023-10-18 18:47:19 +0100

    create posts component

---
+++ app/components/Posts.jsx
@@ -0,0 +1,16 @@
+import { getSortedPostsData } from "@/libs/posts";
+import ListItem from "./ListItem";
+
+export default function Posts() {
+    const posts = getSortedPostsData();
+
+    return (
+        <section className="mt-6 mx-auto max-w-2xl">
+            <ul className="w-full">
+                {posts.map((post) => (
+                    <ListItem key={post.id} post={post} />
+                ))}
+            </ul>
+        </section>
+    );
+}
