fbd587ff0ac78cdbb0e048c4fbe3597c7f1182d3
Will Nickson <wfgnickson@gmail.com>
2023-10-30 11:42:03 +0000

    fix post errors & add get post data function

--- app/libs/posts.jsx
+++ app/libs/posts.jsx
@@ -1,8 +1,10 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
-const postsDirectory = path.join(process.cwd(), "./app/posts");
+const postsDirectory = path.join(process.cwd(), "./app/posts/blog-posts");
 
 export function getSortedPostsData() {
     // Get file names under /posts
@@ -31,4 +33,24 @@ export function getSortedPostsData() {
     return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
 
-export async function getPostData(id) {}
+export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark().use(html).process(matterResult.content);
+
+    const contentHtml = processedContent.toString();
+
+    const blogPostWithHTML = {
+        id,
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        contentHtml,
+    };
+
+    // Combine the data with the id
+    return blogPostWithHTML;
+}
