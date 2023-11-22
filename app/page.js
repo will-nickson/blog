71f409d434a57efbbe1008a3413c1be684a8ced7
Will Nickson <wfgnickson@gmail.com>
2023-11-22 17:12:19 +0000

    update home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import Posts from "./components/Posts";
+import { Posts } from "./posts";
+import { getPosts } from "./get-posts";
 
-export default function Home() {
-    return (
-        <main className="px-6 mx-auto">
-            <p className="mt-12 mb-12 text-3xl text-center">Hello and Welcome 👋</p>
-            <p className="mt-12 mb-12 text-2xl text-center">{"I'm Will"}</p>
-            <Posts />
-        </main>
-    );
+export const revalidate = 60;
+
+export default async function Home() {
+    const posts = await getPosts();
+    return <Posts posts={posts} />;
 }
