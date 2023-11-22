1d88d33a0d0823c388c8f82de363a1e54c0cbae4
Will Nickson <wfgnickson@gmail.com>
2023-11-22 17:11:35 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,65 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withMDX = require("@next/mdx")();
 
-module.exports = nextConfig;
+module.exports = withMDX({
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    experimental: {
+        mdxRs: true,
+    },
+    swcMinify: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pbs.twimg.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "abs.twimg.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "m.media-amazon.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images-na.ssl-images-amazon.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+    headers() {
+        return [
+            {
+                source: "/images/will-nickson.jpg",
+                headers: [
+                    {
+                        key: "cache-control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
+    },
+    redirects() {
+        return [
+            {
+                source: "/essays/:nested*",
+                destination: "/",
+                permanent: true,
+            },
+            {
+                source: "/",
+                destination: "https://github.com/will-nickson",
+                permanent: true,
+            },
+        ];
+    },
+});
