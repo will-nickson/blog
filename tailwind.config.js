2ad5edf1c2fb9515bb050c7bd32270126dc8c18b
Will Nickson <wfgnickson@gmail.com>
2023-11-24 16:31:49 +0000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
+    darkMode: "class",
+    content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-        },
+        extend: {},
     },
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        plugin(function ({ addVariant }) {
+            // this class is applied to `html` by `app/theme-efect.ts`, similar
+            // to how `dark:` gets enabled
+            addVariant("theme-system", ".theme-system &");
+        }),
+    ],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
 };
