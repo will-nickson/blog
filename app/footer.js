c82b3e73913005d108279d5aa452370889e62578
Will Nickson <wfgnickson@gmail.com>
2024-03-02 20:45:34 +0000

    create footer component

---
+++ app/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+            <div className="grow text-left">will@nickson.uk</div>
+        </footer>
+    );
+}
