e59ae70c7d72575c08c6d4a06bc52d1b1dc5f352
Will Nickson <wfgnickson@gmail.com>
2024-03-05 12:20:50 +0000

    update footer

--- app/footer.js
+++ app/footer.js
@@ -1,7 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
     return (
         <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
-            <div className="grow text-left">will@nickson.uk</div>
+            <div className="grow text-left">
+                Will Nickson (
+                <a target="_blank" href="https://twitter.com/will__nickson">
+                    @will__nickson
+                </a>
+                )
+            </div>
+            <div>
+                <a target="_blank" href="https://github.com/will-nickson">
+                    <FaGithub />
+                </a>
+            </div>
         </footer>
     );
 }
